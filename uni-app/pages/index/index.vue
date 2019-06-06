3994e42578cc8e85162c3fbd1b1b8a29c76a5937
liubin198908245711 <391948204@qq.com>
2019-06-06 15:17:47 +0800

    111

--- uni-app/pages/index/index.vue
+++ uni-app/pages/index/index.vue
@@ -335,8 +335,8 @@
 
 					//联网成功的回调,隐藏下拉刷新和上拉加载的状态;
 					//mescroll会根据传的参数,自动判断列表如果无任何数据,则提示空;列表无下一页数据,则提示无更多数据;
-					console.log("mescroll.num=" + mescroll.num + ", mescroll.size=" + mescroll.size + ", curPageData.length=" +
-						curPageData.length);
+					// console.log("mescroll.num=" + mescroll.num + ", mescroll.size=" + mescroll.size + ", curPageData.length=" +
+					// 	curPageData.length);
 
 					//方法一(推荐): 后台接口有返回列表的总页数 totalPage
 					//mescroll.endByPage(curPageData.length, totalPage); //必传参数(当前页的数据个数, 总页数)
@@ -348,14 +348,14 @@
 					//mescroll.endSuccess(curPageData.length, hasNext); //必传参数(当前页的数据个数, 是否有下一页true/false)
 
 					//方法四 (不推荐),会存在一个小问题:比如列表共有20条数据,每页加载10条,共2页.如果只根据当前页的数据个数判断,则需翻到第三页才会知道无更多数据,如果传了hasNext,则翻到第二页即可显示无更多数据.
-					mescroll.endSuccess(curPageData.length);
+					mescroll.endSuccess(curPageData.data.length);
 
 					//设置列表数据
 					if (mescroll.num == 1) that.goodsList = []; //如果是第一页需手动制空列表
-					that.goodsList = that.goodsList.concat(curPageData); //追加新数据
-					that.redList = that.redList;
-					that.hotList = that.hotList;
-					that.topList = that.topList;
+					that.goodsList = that.goodsList.concat(curPageData.data); //追加新数据
+					that.redList = curPageData.redList;
+					that.hotList = curPageData.hotList;
+					that.topList = curPageData.topList;
 
 				}, () => {
 					//联网失败的回调,隐藏下拉刷新的状态
@@ -405,7 +405,7 @@
 								if (res.data.data == null) {
 									res.data.data = [];
 								}
-								successCallback && successCallback(res.data.data);
+								successCallback && successCallback(res.data);
 
 
 
